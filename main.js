c61453abfd4717db5e2062416e52ef085458d21f
FC-HaidarAli <169917859+FC-HaidarAli@users.noreply.github.com>
2024-06-28 08:45:02 -0400

    append obj to array

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const myLibrary = [];
 
 function Book(title, author, numberOfPages, haveRead) {
-
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.haveRead = haveRead;
 }
 
 function addBookToLibrary() {
-    
+    const newBook = new Book("Harry Potter Philosopher's Stone", "J.K Rowling", "223", false);
+    myLibrary.push(newBook);
 }
