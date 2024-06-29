be9a3f3623b0d036925dd22abcf349381550008f
FC-HaidarAli <169917859+FC-HaidarAli@users.noreply.github.com>
2024-06-29 13:37:51 -0400

    added div and test obj

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ function Book(title, author, numberOfPages, haveRead) {
 }
 
 function addBookToLibrary() {
-    //const newBook = new Book("Harry Potter Philosopher's Stone", "J.K Rowling", "223", false);
-    //myLibrary.push(newBook);
     const title = document.querySelector("#title");
     const author = document.querySelector("#author");
     const numberOfPages = document.querySelector("#numberOfPages");
     const haveRead = document.querySelector("#haveRead");
-
+    
     const newBook = new Book(title, author, numberOfPages, haveRead);
     myLibrary.push(newBook);
     console.log(myLibrary[0]);
 }
+const newBook = new Book("Harry Potter Philosopher's Stone", "J.K Rowling", "223", false);
+myLibrary.push(newBook);
 
 const submitButton = document.querySelector("#submitButton");
 submitButton.addEventListener('click', () => {
